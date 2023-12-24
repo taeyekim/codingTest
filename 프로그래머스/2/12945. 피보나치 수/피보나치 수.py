cea9b2b3520b975243bf521a53cdd1d0e4e60cba
taeyekim <127179614+taeyekim@users.noreply.github.com>
2023-12-24 22:05:42 +0900

    [level 2] Title: 피보나치 수, Time: 85.51 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,14 +1,19 @@
 def solution(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    fibo = []
-    for i in range(0, n + 1):
-        if i == 0:
-            fibo.append(0)
-        elif i == 1:
-            fibo.append(1)
-        else:
-            fibo.append(fibo[i-1] + fibo[i-2])
-    return fibo[n] % 1234567
+    # if n == 0:
+    #     return 0
+    # elif n == 1:
+    #     return 1
+    # fibo = []
+    # for i in range(0, n + 1):
+    #     if i == 0:
+    #         fibo.append(0)
+    #     elif i == 1:
+    #         fibo.append(1)
+    #     else:
+    #         fibo.append(fibo[i-1] + fibo[i-2])
+    # return fibo[n] % 1234567
+    a = 0
+    b = 1
+    for i in range(n):
+        a, b = b, a + b
+    return a % 1234567
