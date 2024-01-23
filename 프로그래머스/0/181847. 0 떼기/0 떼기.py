9d2984fbcdb790c798abbc366a04c9deef8df49e
taeyekim <127179614+taeyekim@users.noreply.github.com>
2024-01-23 14:01:33 +0900

    [level 0] Title: 0 떼기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -1,7 +1,2 @@
 def solution(n_str):
-    for i in range(len(n_str)):
-        if n_str[0] == '0':
-            n_str = n_str[1:]
-        else:
-            break
-    return n_str
+    return n_str.lstrip('0')
