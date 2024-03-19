5b153a0fe3193c10116aa5f5d5a1434a91ff0890
taeyekim <127179614+taeyekim@users.noreply.github.com>
2024-03-19 23:20:30 +0900

    [Silver II] Title: 섬의 개수, Time: 156 ms, Memory: 111284 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -8,30 +8,28 @@ dy = [1, -1, 0, 0, 1, -1, -1, 1]
 
 def bfs(row, col):
     lst = []
+    jido[row][col] = 0
     lst.append((row, col))
-
-    visited[row][col] = True
     while lst:
         x, y = lst.pop(0)
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx <= h - 1 and 0 <= ny <= w - 1:
-                if jido[nx][ny] == 1 and visited[nx][ny] == False:
+                if jido[nx][ny] == 1:
                     lst.append((nx, ny))
-                    visited[nx][ny] = True
+                    jido[nx][ny] = 0
 
 while True:
 
     w, h = map(int, input().split())
     if w == 0 and h == 0:
         break
-    visited = [[False] * w for _ in range(h)]
     jido = [list(map(int, input().split())) for _ in range(h)]
     cnt = 0
     for i in range(h):
         for j in range(w):
-            if visited[i][j] == False and jido[i][j] == 1:
+            if jido[i][j] == 1:
                 bfs(i, j)
                 cnt += 1
     print(cnt)
