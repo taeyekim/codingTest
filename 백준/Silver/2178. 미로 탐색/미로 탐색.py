5966362d228e81d91ec3bcdc3247a368a1640a76
taeyekim <127179614+taeyekim@users.noreply.github.com>
2024-04-05 22:03:38 +0900

    [Silver I] Title: 미로 탐색, Time: 152 ms, Memory: 114212 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -5,28 +5,20 @@ dx = [1, -1, 0, 0]
 
 def bfs(col, row):
     q = deque()
-    check_sum = 21e9
-    visited = [[0] * M for _ in range(N)]
-    q.append((col, row, visited))
-    visited[0][0] = 1
+    visited = [[False] * M for _ in range(N)]
+    visited[0][0] = True
+    q.append((col, row, 1))
 
     while q:
-        y, x, v = q.popleft()
+        y, x, cnt = q.popleft()
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-            if 0 <= ny <= N-1 and 0 <= nx <= M-1 and lst[ny][nx] == 1 and v[ny][nx] == 0:
-                v[ny][nx] += v[y][x] + 1
-                # print(ny, nx, v[ny][nx])
-                # for i in range(N):
-                #     print(v[i])
-                # print()
+            if 0 <= ny <= N-1 and 0 <= nx <= M-1 and lst[ny][nx] == 1 and not visited[ny][nx]:
                 if ny == N-1 and nx == M-1:
-                    check_sum = min(check_sum, v[ny][nx])
-                else:
-                    q.append((ny, nx, v))
-
-    return check_sum
+                    return cnt + 1
+                visited[ny][nx] = True
+                q.append((ny, nx, cnt + 1))
 
 N, M = map(int, input().split())
 
