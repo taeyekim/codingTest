95e7fc29e7315b5e4b7dc9d1151503318068cdba
taeyekim <127179614+taeyekim@users.noreply.github.com>
2026-02-19 14:34:39 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,10 +1,14 @@
 def solution(array):
-    answer = []
-    index = 0
-    num = array[0]
-    for i in range(1, len(array)):
-        if num < array[i]:
-            num = array[i]
-            index = i
-    answer = [num, index]
+    # answer = []
+    # index = 0
+    # num = array[0]
+    # for i in range(1, len(array)):
+    #     if num < array[i]:
+    #         num = array[i]
+    #         index = i
+    # answer = [num, index]
+    # return answer
+    
+    val = max(array)
+    answer = [val, array.index(val)]
     return answer
