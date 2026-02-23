a2bd44000ab178ecf95ea24e0bac0ded6e6981e4
taeyekim <127179614+taeyekim@users.noreply.github.com>
2026-02-23 21:22:16 +0900

    [level 2] Title: 타겟 넘버, Time: 372.09 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,16 @@
+
+def solution(numbers, target):
+    answer = 0
+
+    def dfs(index, sum):
+        nonlocal answer
+        if index == len(numbers):
+            if target == sum:
+                answer += 1
+            return
+        dfs(index + 1, sum + numbers[index])
+        dfs(index + 1, sum - numbers[index])
+    
+    dfs(0, 0)
+    
+    return answer
